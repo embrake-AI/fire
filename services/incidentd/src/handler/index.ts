07bbd10d410c6da53e06c74d23c82f3c135afd66
Miquel Puig Turon <miquel.puig@genesy.ai>
2025-12-24 16:30:01 +0100

    huh weird

--- services/incidentd/src/handler/index.ts
+++ services/incidentd/src/handler/index.ts
@@ -22,7 +22,7 @@ export async function startIncident<E extends AuthContext>({
 } & Pick<IS, "prompt" | "createdBy" | "source">) {
 	const clientId = c.var.auth.clientId;
 	const metadata = { ...m, clientId, identifier };
-	const incidentId = c.env.INCIDENT.idFromString(identifier);
+	const incidentId = c.env.INCIDENT.idFromName(identifier);
 	const incident = c.env.INCIDENT.get(incidentId);
 	const startedIncident = await incident.start(
 		{
