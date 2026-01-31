ab5d5553be0fe0e044d992fcf6e60b516592e1d8
Miquel Puig Turon <miquel.puig@genesy.ai>
2026-01-31 19:37:37 +0100

    recreate db every time

--- services/incidentd/src/lib/db.ts
+++ services/incidentd/src/lib/db.ts
@@ -5,12 +5,7 @@ import { drizzle, type NodePgDatabase } from "drizzle-orm/node-postgres";
 type DBSchema = typeof schema;
 type DBRelations = typeof relations;
 
-let db: NodePgDatabase<DBSchema, DBRelations>;
 export function getDB(hyperdrive: Hyperdrive): NodePgDatabase<DBSchema, DBRelations> {
-	if (db) {
-		return db;
-	}
 	// requires pg being installed
-	db = drizzle(hyperdrive.connectionString, { schema, relations });
-	return db;
+	return drizzle(hyperdrive.connectionString, { schema, relations });
 }
