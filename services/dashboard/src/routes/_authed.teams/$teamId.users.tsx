d5c9ae84545bc4bf8d2119b8f16af0d512ee9bd8
Miquel Puig Turon <miquel.puig@genesy.ai>
2026-02-22 23:29:28 +0100

    disallow updating team_member role

--- services/dashboard/src/routes/_authed.teams/$teamId.users.tsx
+++ services/dashboard/src/routes/_authed.teams/$teamId.users.tsx
@@ -9,7 +9,7 @@ import { ConfigCard, ConfigCardActions, ConfigCardContent, ConfigCardDeleteButto
 import { Popover, PopoverContent, PopoverTrigger } from "~/components/ui/popover";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "~/components/ui/select";
 import { Skeleton } from "~/components/ui/skeleton";
-import { useAddSlackUserAsTeamMember, useAddTeamMember, useRemoveTeamMember, useUpdateTeamMemberRole } from "~/lib/teams/teams.hooks";
+import { useAddSlackUserAsTeamMember, useAddTeamMember, useRemoveTeamMember } from "~/lib/teams/teams.hooks";
 import { usePossibleSlackUsers, useUsers } from "~/lib/users/users.hooks";
 
 export const Route = createFileRoute("/_authed/teams/$teamId/users")({
@@ -33,14 +33,9 @@ function TeamUsers(props: { teamId: string }) {
 		setMembers(reconcile(usersQuery.data?.filter((u) => u.teams.some((membership) => membership.id === props.teamId)) ?? [], { key: "id" }));
 	});
 	const removeMemberMutation = useRemoveTeamMember();
-	const updateRoleMutation = useUpdateTeamMemberRole();
 
 	const handleRemoveMember = async (userId: string) => {
 		removeMemberMutation.mutate({ teamId: props.teamId, userId });
-	};
-
-	const handleUpdateRole = (userId: string, role: "MEMBER" | "ADMIN") => {
-		updateRoleMutation.mutate({ teamId: props.teamId, userId, role });
 	};
 
 	return (
@@ -72,20 +67,6 @@ function TeamUsers(props: { teamId: string }) {
 									<ConfigCardContent>
 										<ConfigCardTitle>{member.name}</ConfigCardTitle>
 									</ConfigCardContent>
-									<Select
-										value={member.teams.find((membership) => membership.id === props.teamId)?.role ?? "MEMBER"}
-										onChange={(value) => {
-											if (!value) return;
-											handleUpdateRole(member.id, value as "MEMBER" | "ADMIN");
-										}}
-										options={["ADMIN", "MEMBER"]}
-										itemComponent={(props) => <SelectItem item={props.item}>{props.item.rawValue === "ADMIN" ? "Admin" : "Member"}</SelectItem>}
-									>
-										<SelectTrigger class="h-8 w-28">
-											<SelectValue<string>>{(state) => (state.selectedOption() === "ADMIN" ? "Admin" : "Member")}</SelectValue>
-										</SelectTrigger>
-										<SelectContent />
-									</Select>
 									<ConfigCardActions animated>
 										<ConfigCardDeleteButton
 											onDelete={() => handleRemoveMember(member.id)}
